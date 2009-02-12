b1a963877557c9d67c807f3f88ad132e0790e90e
rgrp <rgrp@localhost>
2009-02-12 10:08:43 +0000

    [misc][s]: fix gmail tests and put in Repository object to do db create/clean stuff (should have been in rce69abb5af05).

--- isitopen/model/__init__.py
+++ isitopen/model/__init__.py
@@ -1,1 +1,20 @@
+from meta import *
 from enquiry import *
+
+class Repository(object):
+    def __init__(self, ourmetadata, oursession):
+        self.metadata = ourmetadata
+        self.session = oursession
+
+    def create_db(self):
+        self.metadata.create_all(bind=self.metadata.bind)    
+
+    def clean_db(self):
+        self.metadata.drop_all(bind=self.metadata.bind)
+
+    def rebuild_db(self):
+        self.clean_db()
+        self.create_db()
+
+repo = Repository(metadata, Session)
+
