ea360f76f7a33a3e35d3f0aa78100cbbe958b2ae
Nick Stenning <3602+nickstenning@users.noreply.github.com>
2009-03-10 16:28:27 +0000

    Upgrade to Pylons 0.9.7rc6. This appears to work. Possible regression: error status pages (404 and the like). Have also pulled in the [deprecated] rails helpers for the time being.

--- isitopen/lib/helpers.py
+++ isitopen/lib/helpers.py
@@ -3,7 +3,15 @@
 Consists of functions to typically be used within templates, but also
 available to Controllers. This module is available to both as 'h'.
 """
-from webhelpers import *
+from webhelpers.html import escape, HTML, literal, url_escape
+from webhelpers.html.tags import *
+
+# TODO: remove the deprecated calls and replace with functionality from
+# webhelpers.html, etc.
+from webhelpers.rails.wrapped import *
+
+from webhelpers.markdown import markdown
+from routes import url_for
 
 # redo markdown to deal with None values
 old_markdown = markdown
