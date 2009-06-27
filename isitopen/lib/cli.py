75aed3a7b58e6bb36d3a96f068bfcb1005b78bd5
Rufus Pollock <180658+rufuspollock@users.noreply.github.com>
2009-06-27 16:44:14 +0100

    [setup][xs]: move template ini from .egg-info to isitopen/config (as is standard for pylons >= 0.9.7).

---
+++ isitopen/lib/cli.py
@@ -0,0 +1,96 @@
+import os
+import email as E
+
+import paste.script
+
+class Command(paste.script.command.Command):
+    parser = paste.script.command.Command.standard_parser(verbose=True)
+    parser.add_option('-c', '--config', dest='config',
+            default='development.ini', help='Config file to use.')
+    parser.add_option('-f', '--file',
+        action='store',
+        dest='file_path',
+        help="File to dump results to (if needed)")
+    default_verbosity = 1
+    group_name = 'isitopen'
+
+    def _load_config(self):
+        from paste.deploy import appconfig
+        from isitopen.config.environment import load_environment
+        if not self.options.config:
+            msg = 'No config file supplied'
+            raise self.BadCommand(msg)
+        self.filename = os.path.abspath(self.options.config)
+        conf = appconfig('config:' + self.filename)
+        load_environment(conf.global_conf, conf.local_conf)
+
+    def _setup_app(self):
+        cmd = paste.script.appinstall.SetupCommand('setup-app') 
+        cmd.run([self.filename]) 
+
+
+class ManageDb(Command):
+    '''Perform various tasks on the database.
+    
+    db create # create tables
+    db clean
+    db rebuild # clean + create
+    '''
+    summary = __doc__.split('\n')[0]
+    usage = __doc__
+    max_args = None
+    min_args = 1
+
+    def command(self):
+        self._load_config()
+        from isitopen import model
+
+        cmd = self.args[0]
+        if cmd == 'create':
+            model.repo.create_db()
+        elif cmd == 'clean':
+            model.repo.clean_db()
+        elif cmd == 'rebuild':
+            model.repo.clean_db()
+            model.repo.create_db()
+        else:
+            print 'Command %s not recognized' % cmd
+
+class Fixtures(Command):
+    '''Create some fixture data.
+    
+    '''
+    summary = __doc__.split('\n')[0]
+    usage = __doc__
+    max_args = None
+    min_args = 0
+
+    user_email = u'testing@isitopen.org'
+    to = u'testing@enquiries.com'
+    email = E.message_from_string(u'')
+
+    def command(self):
+        self._load_config()
+        self.create()
+        print 'Created fixtures'
+
+    @classmethod
+    def create(self):
+        from isitopen import model
+        user = model.User(email=self.user_email)
+        enq = model.Enquiry(owner=user)
+        self.email['To'] = self.to
+        self.email['Subject'] = u'testing email'
+        mess = model.Message(enquiry=enq)
+        mess.mimetext = self.email.as_string()
+        model.Session.commit()
+        enq_id = enq.id
+        mess_id = mess.id
+        model.Session.remove()
+        return (enq_id, mess_id)
+
+    @classmethod
+    def remove(self):
+        from isitopen import model
+        model.repo.rebuild_db()
+
---
+++ isitopen/tests/functional/test_enquiry.py
@@ -0,0 +1,46 @@
+from isitopen.tests import *
+
+class TestHomeController(TestController):
+    @classmethod
+    def setup_class(self):
+        self.enq_id, self.msg_id = Fixtures.create()
+        self.message = model.Message.query.get(self.msg_id)
+
+    @classmethod
+    def teardown_class(self):
+        Fixtures.remove()
+
+    def test_view(self):
+        offset = url_for(controller='enquiry', action='view',
+                id=self.enq_id)
+        res = self.app.get(offset)
+        print str(res)
+        assert 'Is It Open Data?' in res
+        assert 'Status:' in res
+
+    def test_list(self):
+        offset = url_for(controller='enquiry', action='list')
+        res = self.app.get(offset)
+        assert self.enq_id  in res
+
+    def test_create(self):
+        offset = url_for(controller='enquiry', action='create')
+        res = self.app.get(offset)
+        assert 'Create' in res
+        to = 'xyz@journal.org'
+        sender = 'enquirer@okfn.org'
+        form = res.forms[0]
+        form['to'] = to
+        form['sender'] = sender
+        res = form.submit('preview')
+        assert 'Preview' in res
+        res = form.submit('send')
+        # 302 redirect
+        res = res.follow()
+        assert 'Sent' in res
+        model.Session.remove()
+        assert model.Enquiry.query.count() == 2
+        assert model.Message.query.count() == 2
+
+        # TODO: test bad entry (e.g. no to address)
+
