63a02434d89031eb0e2fdabf2316025ccf5c3a03
rgrp <rgrp@localhost>
2009-07-10 07:45:37 +0100

    [wui,helpers][s]: new helpers email_body method to render body of emails (in addition to markdown, deal with quoting, line continuations etc).

--- isitopen/lib/helpers.py
+++ isitopen/lib/helpers.py
@@ -6,16 +6,24 @@ available to Controllers. This module is available to both as 'h'.
 from webhelpers.html import escape, HTML, literal, url_escape
 from webhelpers.html.tags import *
 
-from webhelpers.markdown import markdown
+from webhelpers.markdown import markdown as old_markdown
 from routes import url_for
 from routes import redirect_to
 
 # redo markdown to deal with None values
-old_markdown = markdown
 def new_markdown(text, **kwargs):
     if not text:
         return ''
     else:
         return old_markdown(text, **kwargs)
 markdown = new_markdown
+
+def email_body(body):
+    body = body.replace('\r\n', '\n')
+    # fix issue where quoted text immediately after quoted text at start of
+    # quoting
+    body = body.replace(':\n>', ':\n\n>')
+    # in quoted sections seem to get line continuations with =
+    body = body.replace('=\n', '')
+    return markdown(body)
 
