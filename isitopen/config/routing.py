b8a7b2a75a199f62a7a70788b497d1b64dcd6ce6
Rufus Pollock <180658+rufuspollock@users.noreply.github.com>
2009-07-16 02:01:31 +0100

    [routing][xs]: standardize on trailing slash on urls.

--- isitopen/config/routing.py
+++ isitopen/config/routing.py
@@ -12,19 +12,24 @@ def make_map():
     """Create, configure and return the routes Mapper"""
     map = Mapper(directory=config['pylons.paths']['controllers'],
                  always_scan=config['debug'])
+    map.minimization = False
 
     # The ErrorController route (handles 404/500 error pages); it should
     # likely stay at the top, ensuring it can always be resolved
-    map.connect('error/:action/:id', controller='error')
+    map.connect('/error/{action}', controller='error')
+    map.connect('/error/{action}/{id}', controller='error')
 
     # CUSTOM ROUTES HERE
-    map.connect('home', '', controller='home', action='index')
-    map.connect('about', 'about', controller='home', action='guide')
-    map.connect('guide', 'guide', controller='home', action='guide')
+    map.connect('home', '/', controller='home', action='index')
+    map.connect('about', '/about/', controller='home', action='guide')
+    map.connect('guide', '/guide/', controller='home', action='guide')
     # Map the /admin url to FA's AdminController
     maps.admin_map(map, controller='admin', url='/admin')
-
-    map.connect(':controller/:action/:id')
-    map.connect('*url', controller='template', action='view')
+    # standard controllers
+    map.connect('/{controller}/', action='index')
+    map.connect('/{controller}/{action}/')
+    map.connect('/{controller}/{action}/{id}/')
+    # standardize on trailing slash
+    map.redirect('/*(url)', '/{url}/', requirements=dict(url='.*[^/]$'), _redirect_code='301 Moved Permanently')
 
     return map
