193e585b21c20c751dad3a2ae7546ff7d942c9fc
Rufus Pollock <180658+rufuspollock@users.noreply.github.com>
2009-08-16 19:49:34 +0100

    [model][m]: start using sqlalchemy-migrate to handle db schema changes.
      * Put current model into migration as first script: 001_add_existing_tables.py
      * Quite a lot of work (though not much code!) updating cli/Repository code to work with migration stuff
        * new db upgrade method
        * discover that cannot run model of migration code/db (using autoload) but have to keep a full 'HEAD' version of table definitions in the model code
      * isitopen/model/user.py: remove as unused

--- isitopen/model/__init__.py
+++ isitopen/model/__init__.py
@@ -1,17 +1,49 @@
 from meta import *
 from enquiry import *
-from user import *
 
+
+import isitopen.migration
 class Repository(object):
+    migrate_repository = isitopen.migration.__path__[0]
+
     def __init__(self, ourmetadata, oursession):
         self.metadata = ourmetadata
         self.session = oursession
 
     def create_db(self):
         self.metadata.create_all(bind=self.metadata.bind)    
+        self.setup_migration_version_control(self.latest_migration_version())
+        # creation this way worked fine for normal use but failed on test with
+        # OperationalError: (OperationalError) no such table: user 
+        # self.upgrade_db()
 
     def clean_db(self):
         self.metadata.drop_all(bind=self.metadata.bind)
+
+    def latest_migration_version(self):
+        import migrate.versioning.api as mig
+        version = mig.version(self.migrate_repository)
+        return version
+
+    def setup_migration_version_control(self, version=None):
+        import migrate.versioning.exceptions
+        import migrate.versioning.api as mig
+        # set up db version control (if not already)
+        try:
+            mig.version_control(self.metadata.bind.url,
+                    self.migrate_repository, version)
+        except migrate.versioning.exceptions.DatabaseAlreadyControlledError:
+            pass
+
+    def upgrade_db(self, version=None):
+        '''Upgrade db using sqlalchemy migrations.
+
+        @param version: version to upgrade to (if None upgrade to latest)
+        '''
+        import migrate.versioning.api as mig
+        self.setup_migration_version_control()
+        mig.upgrade(self.metadata.bind.url, self.migrate_repository,
+                version=version)
 
     def rebuild_db(self):
         self.clean_db()
